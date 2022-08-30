8bfa88713d5a757957dc1dd5f4f528960517475d
gnaina722 <112470009+gnaina722@users.noreply.github.com>
2022-08-30 13:22:34 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,18 @@
+print("hello")
+a=10
+b=20
+c=a+b
+print(c)
+num1 = input('Enter first number: ')  
+num2 = input('Enter second number: ')
+sum = float(num1) + float(num2)  
+sub = float(num1)-float(num2)
+print(sub)
+
+
+
+num = int(input("Enter a number: "))  
+if (num % 2) == 0:  
+   print("{0} is Even number".format(num))  
+else:  
+   print("{0} is Odd number".format(num))  
